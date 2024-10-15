36812649c067e5f15ed1730a27c5cc5746e0ea43
OrellanaPc <astridofelia@gmial.com>
2024-10-15 13:39:50 -0600

    Se creó Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Login from "./Login";
+import './index.css';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Login/>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    'src/**/*.{js,jsx,ts,tsx}', // Make sure your paths are correct
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('@tailwindcss/forms'), // This line ensures that the forms plugin is used
+  ],
+};
